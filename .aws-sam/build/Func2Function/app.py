7a035b8850fe43b2f656ba66ea6053d848cdf492
nivekiba <nivekiba@node-000.nivekiba-267803.ntu-cloud-pg0.wisc.cloudlab.us>
2025-09-09 08:01:07 -0500

    modif

--- .aws-sam/build/Func2Function/app.py
+++ .aws-sam/build/Func2Function/app.py
@@ -3,8 +3,12 @@ import time
 def lambda_handler(event, context):
     sleep_time_ms = event['sleep_time_ms']
 
-    time.sleep(sleep_time_ms/1000)
+    loops = event.get('loops', 30000000)  # number of iterations
+    # Busy-spin loop to simulate delay without sleeping
+    result = 0
+    for i in range(loops):
+        result += i * (i + 1)
 
     return {
-        "sleep_time_ms": sleep_time_ms
+        "loops": loops
     }
